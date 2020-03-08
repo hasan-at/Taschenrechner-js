dea986e75ffc199946c31dcc7962b72c0fd99c49
Hasan <hassn.atli.1@gmail.com>
2020-03-08 17:30:59 +0100

    js-vr1

---
+++ assets/js/mein.js
@@ -0,0 +1,14 @@
+function Reschnen(number){
+    document.getElementById("texst").innerHTML += number
+}
+function Reschnen16(){
+    document.getElementById("texst").innerHTML = "";
+}
+function Reschnen1(re){
+    document.getElementById("texst").innerHTML += re;
+
+}
+function Reschnen15(){
+document.getElementById("texst").innerHTML = eval(document.getElementById("texst").innerHTML )
+
+}
